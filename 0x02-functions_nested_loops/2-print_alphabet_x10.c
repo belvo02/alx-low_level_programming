92d9aba12ebbe8656759240c9cef4f1e1df6f1a2
belvo02 <belvo02@gmail.com>
2023-02-21 19:36:56 -0800

    0x002-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* Print alphabet x 10 */
+
+void print_alphabet_x10(void);
+{
+	int ten;
+	char du;
+for (ten = 0; ten <= 9, ten++)
+	{
+	for (du = 'a'; du <= 'z'; du++)
+	{
+		_putchar(du);
+	_putchar('\n');
+	}
+	}
+}
+
