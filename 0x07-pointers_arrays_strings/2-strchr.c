2b7743e33cae1165e2469b09e0a25ee88b689ce6
belvo02 <belvo02@gmail.com>
2023-03-07 02:37:04 +0100

    my 0x07 pointers arrays and strings work

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,25 +1,26 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _strchr - prints from the first occurrence of a char.
- * @s: source string
- * @c: tested char
+ * _strchr - locates a character in a string
  *
- * Return: new string.
+ * @c: occurrence of the character
+ *
+ * @s: in the string
+ *
+ * Return: a pointer to the first occurrence of the character
  */
+
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == c)
-		{
-			return (s);
-
-		}
-		else
+		if (*s != c)
 			s++;
+		else
+			return (s);
 	}
-	if (*s == c)
+	if (c == '\0')
 		return (s);
-	else
-		return (0);
+
+	return (NULL);
 }
