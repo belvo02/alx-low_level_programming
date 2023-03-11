f1ab3e8cff93d3cb536407d69f21abec54a02659
belvo02 <belvo02@gmail.com>
2023-03-11 12:07:05 +0100

    0x09-static_libraries

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <unistd.h>
+/*_putchar writes C to stdout
+*@c: the character to print
+*Return if successful: On succes 1. on error -1*/
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
