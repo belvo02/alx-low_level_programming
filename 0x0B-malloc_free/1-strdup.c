83e8e782ba55b2bcfd747c049a38363a21b47033
belvo02 <belvo02@gmail.com>
2023-03-14 17:30:08 +0100

    Malloc begins

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * _strdup - duplicate string to new pointer
+ * @str: string to duplicate
+ *
+ * Return: pointer which duplicates string on sucess
+ * and NULL if string is null or memory error occurs,
+ **/
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, len;
+
+	i = 0;
+	dup = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[dup])
+		dup++;
+		duplicate = malloc(sizeof(char) * (dup+1));
+	if (duplicate == NULL)
+	return (NULL);
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
+}
